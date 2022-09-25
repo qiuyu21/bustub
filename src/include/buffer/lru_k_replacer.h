7b61b4588dac9d26b3c2a3b1c47b1c147ba8c901
qiuyu21 <38485507+qiuyu21@users.noreply.github.com>
2022-09-25 18:05:22 +1000

    lru k

--- src/include/buffer/lru_k_replacer.h
+++ src/include/buffer/lru_k_replacer.h
@@ -17,6 +17,8 @@
 #include <mutex>  // NOLINT
 #include <unordered_map>
 #include <vector>
+#include <map>
+#include <iostream>
 
 #include "common/config.h"
 #include "common/macros.h"
@@ -130,16 +132,71 @@ class LRUKReplacer {
    *
    * @return size_t
    */
-  auto Size() -> size_t;
+  inline auto Size() -> size_t { return curr_size_; }
 
- private:
-  // TODO(student): implement me! You can replace these member variables as you like.
-  // Remove maybe_unused if you start using them.
-  [[maybe_unused]] size_t current_timestamp_{0};
-  [[maybe_unused]] size_t curr_size_{0};
-  [[maybe_unused]] size_t replacer_size_;
-  [[maybe_unused]] size_t k_;
-  std::mutex latch_;
+
+  class Frame {
+    public:
+
+      explicit Frame(frame_id_t, size_t);
+
+      auto inline getFrameId() const -> frame_id_t { return id_; }
+
+      auto inline getEvitable() const -> bool { return evitable_;  }
+
+      auto inline setEvitable(bool b) { evitable_ = b; }
+
+      auto inline getTimeStamps() -> std::vector<size_t> & { return timestamps_; }
+
+      auto inline getTimeStamp() -> size_t { return timestamps_.at(0); }
+
+      void addTimeStamp(size_t timestamp);
+
+      auto inline getIndex() const -> size_t { return i_; }
+
+      auto inline setIndex(size_t i) { i_ = i; }
+      
+    private:
+
+      frame_id_t id_;
+      bool evitable_;
+      std::vector<size_t> timestamps_;
+      size_t k_;
+      size_t i_;
+  };
+
+  void addTimeStamp(std::shared_ptr<Frame>, size_t);
+
+  auto pop() -> std::shared_ptr<Frame>;
+
+  void push(frame_id_t frame_id);
+
+  //
+  void swap(bool b, size_t i, size_t j);
+
+  void up(bool b, size_t i);
+
+  void down(bool b, size_t i);
+
+  auto remove(bool b, size_t i) -> std::shared_ptr<Frame>;
+
+  auto less(bool b, size_t i, size_t j) -> bool;
+
+  private:
+    size_t current_timestamp_{0};
+    size_t curr_size_{0};       // number of evitable frames
+    size_t replacer_size_;      // maximum number of evitable frames
+    size_t k_;
+    
+    std::mutex latch_;
+
+    // std::map<frame_id_t, std::shared_ptr<Frame>> frames_;
+
+    std::vector<std::shared_ptr<Frame>> frames_;
+
+    std::vector<std::shared_ptr<Frame>> minheap_L_; // Min heap storing frames with < K references
+
+    std::vector<std::shared_ptr<Frame>> minheap_G_; // Min heap storing frames with >= K references
 };
 
 }  // namespace bustub
